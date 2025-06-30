587df3d7781f845d38707e3f50ec826e3d100765
Béranger THOMAS <beranger.t@proton.me>
2025-06-30 15:44:28 +0200

    Modularisation du reporting

---
+++ reporting/__init__.py
@@ -0,0 +1,8 @@
+"""
+Module de reporting pour l'extraction d'horaires.
+Contient les gestionnaires pour la génération et l'envoi de rapports.
+"""
+
+from .report_manager import ReportManager
+
+__all__ = ["ReportManager"]
